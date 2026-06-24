1ffdbddb2c9219ac162f38097d38257b84f37b0a
jabu40237-coder <jabu40237@gmail.com>
2026-06-24 18:52:09 +0300

    Initialize Firebase app with configuration and services

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyA7eAsEmQO2UNVi2A0Z1BX2qd0m3SxJG3I",
+    authDomain: "lokxse-db.firebaseapp.com",
+    projectId: "lokxse-db",
+    storageBucket: "lokxse-db.firebasestorage.app",
+    messagingSenderId: "207391415812",
+    appId: "1:207391415812:web:545ff064d5691be52e5f51"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
